f765f9f9ca1f084d382cb513987a26b8cbb6062e
bvd <vandenabeele.bastiaan@gmail.com>
2026-02-01 17:38:09 +0100

    format settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,5 +1,6 @@
-from pydantic_settings import BaseSettings
 from functools import lru_cache
+
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -30,7 +31,7 @@ class Settings(BaseSettings):
         env_file_encoding = "utf-8"
 
 
-@lru_cache()
+@lru_cache
 def get_settings() -> Settings:
     return Settings()
 
