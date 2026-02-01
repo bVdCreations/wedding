ce0235a84779417e11ec1e5d9be866bb767f1dd4
bvd <vandenabeele.bastiaan@gmail.com>
2026-02-01 18:23:06 +0100

    format

--- src/email/service.py
+++ src/email/service.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from typing import Optional
+
 from src.config.settings import settings
 from src.email.templates import EmailTemplates
 
