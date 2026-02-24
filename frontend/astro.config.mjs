9d619039dde303c73517366d00b901af0dba2be4
bvd <vandenabeele.bastiaan@gmail.com>
2026-02-24 13:30:41 +0100

    set redirect in config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'static',
   integrations: [tailwind()],
+  redirects: {
+    '/': '/en',
+  },
   server: {
     port: 4321,
     host: true,
